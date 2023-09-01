4bdf507fd2d6f8c1f39774257e427d3918aad07a
Bianca Nenciu <nenciu.bianca@gmail.com>
2023-09-01 15:07:28 +0300

    Write a LICENSE file

--- lib/discourse_theme/scaffold.rb
+++ lib/discourse_theme/scaffold.rb
@@ -20,6 +20,28 @@ module DiscourseTheme
       Are you a bit lost? Be sure to read https://meta.discourse.org/t/how-to-develop-custom-themes/60848
     STR
 
+    LICENSE = <<~STR
+      Copyright #YEAR #AUTHOR
+
+      Permission is hereby granted, free of charge, to any person obtaining a copy
+      of this software and associated documentation files (the "Software"), to deal
+      in the Software without restriction, including without limitation the rights
+      to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+      copies of the Software, and to permit persons to whom the Software is
+      furnished to do so, subject to the following conditions:
+
+      The above copyright notice and this permission notice shall be included in all
+      copies or substantial portions of the Software.
+
+      THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+      IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+      FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+      AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+      LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+      OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+      SOFTWARE.
+    STR
+
     GIT_IGNORE = <<~STR
       .discourse-site
       node_modules
@@ -106,6 +128,9 @@ module DiscourseTheme
 
         UI.info "Creating HELP"
         File.write('HELP', HELP)
+
+        UI.info "Creating LICENSE"
+        File.write('LICENSE', LICENSE.sub("#YEAR", "#{Date.today.year}").sub("#AUTHOR", author))
 
         UI.info "Creating package.json"
         File.write('package.json', PACKAGE_JSON.sub("#AUTHOR", author))
