104f4cb634300be1d24f39b1fda4f1f2cbace2e0
Bianca Nenciu <nbianca@users.noreply.github.com>
2023-09-06 11:46:32 +0300

    DEV: Various usability improvements (#27)
    * Add LICENSE and settings.yml files
    
    * Add TODOs for about_url and license_url
    
    * Update API version in initializer
    
    * Initialize new repository with main branch
    
    The previous command line worked just for Git v2.28 and up.
    
    * Bump version to 0.7.7

--- lib/discourse_theme/scaffold.rb
+++ lib/discourse_theme/scaffold.rb
@@ -1,24 +1,41 @@
 # frozen_string_literal: true
 
+require 'date'
 require 'json'
 
 module DiscourseTheme
   class Scaffold
 
-    BLANK_FILES = %w{
-      common/common.scss
-
-      settings.yml
-    }
-
     ABOUT_JSON = {
-      about_url: nil,
-      license_url: nil,
+      about_url: "TODO: Put your theme's public repo or Meta topic URL here",
+      license_url: "TODO: Put your theme's LICENSE URL here",
       assets: {}
     }
 
     HELP = <<~STR
       Are you a bit lost? Be sure to read https://meta.discourse.org/t/how-to-develop-custom-themes/60848
+    STR
+
+    LICENSE = <<~STR
+      Copyright #YEAR #AUTHOR
+
+      Permission is hereby granted, free of charge, to any person obtaining a copy
+      of this software and associated documentation files (the "Software"), to deal
+      in the Software without restriction, including without limitation the rights
+      to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+      copies of the Software, and to permit persons to whom the Software is
+      furnished to do so, subject to the following conditions:
+
+      The above copyright notice and this permission notice shall be included in all
+      copies or substantial portions of the Software.
+
+      THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+      IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+      FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+      AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+      LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+      OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+      SOFTWARE.
     STR
 
     GIT_IGNORE = <<~STR
@@ -30,7 +47,7 @@ module DiscourseTheme
     API_INITIALIZER = <<~STR
       import { apiInitializer } from "discourse/lib/api";
 
-      export default apiInitializer("0.11.1", api => {
+      export default apiInitializer("1.8.0", (api) => {
         console.log("hello world from api initializer!");
       });
     STR
@@ -68,6 +85,11 @@ module DiscourseTheme
           description: "#DESCRIPTION"
     YAML
 
+    SETTINGS_YML = <<~YAML
+      todo_rename_and_use_setting:
+        default: ""
+    YAML
+
     def self.generate(dir)
       UI.progress "Generating a scaffold theme at #{dir}"
 
@@ -95,7 +117,6 @@ module DiscourseTheme
 
         description = UI.ask("How would you describe this theme?").to_s.strip
 
-        UI.info "Creating about.json"
         about_template = ABOUT_JSON.dup
         about_template[:name] = name
         if is_component
@@ -103,46 +124,35 @@ module DiscourseTheme
         else
           about_template[:color_schemes] = {}
         end
-        File.write('about.json', JSON.pretty_generate(about_template))
-
-        UI.info "Creating HELP"
-        File.write('HELP', HELP)
-
-        UI.info "Creating package.json"
-        File.write('package.json', PACKAGE_JSON.sub("#AUTHOR", author))
-
-        UI.info "Creating .template-lintrc.js"
-        File.write('.template-lintrc.js', TEMPLATE_LINT_RC)
-
-        UI.info "Creating .eslintrc"
-        File.write('.eslintrc', ESLINT_RC)
-
-        UI.info "Creating .gitignore"
-        File.write('.gitignore', GIT_IGNORE)
-
-        locale = "locales/en.yml"
-        UI.info "Creating #{locale}"
-        FileUtils.mkdir_p(File.dirname(locale))
-        File.write(locale, EN_YML.sub("#DESCRIPTION", description))
 
         encoded_name = name.downcase.gsub(/[^a-zA-Z0-9_-]+/, '_')
-        initializer = "javascripts/discourse/api-initializers/#{encoded_name}.js"
-        UI.info "Creating #{initializer}"
-        FileUtils.mkdir_p(File.dirname(initializer))
-        File.write(initializer, API_INITIALIZER)
 
-        BLANK_FILES.each do |f|
-          UI.info "Creating #{f}"
-          FileUtils.mkdir_p File.dirname(f)
-          FileUtils.touch f
-        end
+        write('about.json', JSON.pretty_generate(about_template))
+        write('HELP', HELP)
+        write('LICENSE', LICENSE.sub("#YEAR", "#{Date.today.year}").sub("#AUTHOR", author))
+        write('.eslintrc', ESLINT_RC)
+        write('.gitignore', GIT_IGNORE)
+        write('.template-lintrc.js', TEMPLATE_LINT_RC)
+        write('package.json', PACKAGE_JSON.sub("#AUTHOR", author))
+        write('settings.yml', SETTINGS_YML)
+        write('common/common.scss', '')
+        write("javascripts/discourse/api-initializers/#{encoded_name}.js", API_INITIALIZER)
+        write('locales/en.yml', EN_YML.sub("#DESCRIPTION", description))
 
         UI.info "Initializing git repo"
-        puts `git init . --initial-branch=main`
+        puts `git init && git symbolic-ref HEAD refs/heads/main`
 
         UI.info "Installing dependencies"
         puts `yarn`
       end
+    end
+
+    private
+
+    def self.write(filename, contents)
+      UI.info "Creating #{filename}"
+      FileUtils.mkdir_p(File.dirname(filename))
+      File.write(filename, contents)
     end
   end
 end
